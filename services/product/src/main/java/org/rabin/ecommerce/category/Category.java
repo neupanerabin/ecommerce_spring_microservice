97518e8c31ef3b157a14fa8263fef55554b1b061
neupanerabin7@gmail.com <neupanerabin7@gmail.com>
2024-08-09 23:46:47 +0545

    add database concepts

---
+++ services/product/src/main/java/org/rabin/ecommerce/category/Category.java
@@ -0,0 +1,29 @@
+package org.rabin.ecommerce.category;
+
+
+/*
+ * @author : rabin
+ */
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.rabin.ecommerce.product.Product;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+public class Category {
+    @Id@GeneratedValue
+    private Integer id;
+    private String name;
+    private String description;
+    @OneToMany(mappedBy = "category", cascade = CascadeType.REMOVE)
+    private List<Product> products;
+
+}
---
+++ services/product/src/main/java/org/rabin/ecommerce/product/Product.java
@@ -0,0 +1,34 @@
+package org.rabin.ecommerce.product;
+
+
+/*
+ * @author : rabin
+ */
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.rabin.ecommerce.category.Category;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String name;
+    private String description;
+    private double availableQuantity;
+    private BigDecimal price;
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+
+}
