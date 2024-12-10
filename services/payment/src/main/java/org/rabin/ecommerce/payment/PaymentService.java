778cd08f3fae630e8536b8497fb6d8c19ed6bbee
neupanerabin7@gmail.com <neupanerabin7@gmail.com>
2024-12-10 23:23:51 +0545

    add payment service

--- services/payment/src/main/java/org/rabin/ecommerce/payment/PaymentService.java
+++ services/payment/src/main/java/org/rabin/ecommerce/payment/PaymentService.java
@@ -6,6 +6,8 @@ package org.rabin.ecommerce.payment;
  */
 
 import lombok.RequiredArgsConstructor;
+import org.rabin.ecommerce.notification.NotificationProducer;
+import org.rabin.ecommerce.notification.PaymentNotificationRequest;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,9 +15,21 @@ import org.springframework.stereotype.Service;
 public class PaymentService {
     private  final PaymentRepository repository;
     private final PaymentMapper mapper;
+    private final NotificationProducer notificationProducer;
 
     public Integer createPayment(PaymentRequest request) {
         var payment = repository.save(mapper.toPayment(request));
-        return null;
+        notificationProducer.sendNotification(
+                new PaymentNotificationRequest(
+                        request.orderRefrence(),
+                        request.paymentMethod(),
+                        request.customer().firstname(),
+                        request.customer().lastname(),
+                        request.customer().email()
+
+                )
+        );
+
+        return payment.getId();
     }
 }
