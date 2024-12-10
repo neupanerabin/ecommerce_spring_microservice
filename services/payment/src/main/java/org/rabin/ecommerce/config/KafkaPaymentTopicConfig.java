92d2d35d7b6522b04b657e912cb1e3c007e8284e
neupanerabin7@gmail.com <neupanerabin7@gmail.com>
2024-12-10 23:09:13 +0545

    add service notification

---
+++ services/payment/src/main/java/org/rabin/ecommerce/config/KafkaPaymentTopicConfig.java
@@ -0,0 +1,23 @@
+package org.rabin.ecommerce.config;
+
+
+/*
+ * @author : rabin
+ */
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaPaymentTopicConfig {
+
+    @Bean
+    public NewTopic paymentTopic(){
+        return TopicBuilder
+                .name("payment-topic")
+                .build();
+
+    }
+}
