918c9bd2c85a77b764218d755ecec1bbd8f25802
neupanerabin7@gmail.com <neupanerabin7@gmail.com>
2024-12-11 23:23:42 +0545

    add payment Notification

--- services/payment/src/main/java/org/rabin/ecommerce/notification/PaymentNotificationRequest.java
+++ services/payment/src/main/java/org/rabin/ecommerce/notification/PaymentNotificationRequest.java
@@ -7,8 +7,11 @@ package org.rabin.ecommerce.notification;
 
 import org.rabin.ecommerce.payment.PaymentMethod;
 
+import java.math.BigDecimal;
+
 public record PaymentNotificationRequest(
         String orderReference,
+        BigDecimal amount,
         PaymentMethod paymentMethod,
         String customerFirstName,
         String customerLastName,
