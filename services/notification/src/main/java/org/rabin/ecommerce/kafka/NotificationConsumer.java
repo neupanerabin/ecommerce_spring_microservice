d65d68dc04ae888de410483ffc2e032bd1cd1e56
neupanerabin7@gmail.com <neupanerabin7@gmail.com>
2024-12-12 22:52:07 +0545

    add email setup to notification

--- services/notification/src/main/java/org/rabin/ecommerce/kafka/NotificationConsumer.java
+++ services/notification/src/main/java/org/rabin/ecommerce/kafka/NotificationConsumer.java
@@ -5,8 +5,10 @@ package org.rabin.ecommerce.kafka;
  * @author : rabin
  */
 
+import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.rabin.ecommerce.email.EmailService;
 import org.rabin.ecommerce.kafka.order.OrderConfirmation;
 import org.rabin.ecommerce.kafka.payment.PaymentConfirmation;
 import org.rabin.ecommerce.notification.Notification;
@@ -25,10 +27,10 @@ import static org.rabin.ecommerce.notification.NotificationType.PAYMENT_CONFIRMA
 public class NotificationConsumer {
 
     private final NotificationRepository repository;
-    // private final EmailService emailService;
+     private final EmailService emailService;
 
     @KafkaListener(topics = "payment-topic")
-    public void consumePaymentSuccessNotification(PaymentConfirmation paymentConfirmation){
+    public void consumePaymentSuccessNotification(PaymentConfirmation paymentConfirmation) throws MessagingException {
         log.info(String.format("Consuming the message from payment-topic Topic:: %s", paymentConfirmation));
         repository.save(
                 Notification.builder()
@@ -39,11 +41,19 @@ public class NotificationConsumer {
         );
 
         // to do send email
+        var customerName = paymentConfirmation.customerFirstname() + " " + paymentConfirmation.customerLastname();
+        emailService.sendPaymentSuccessEmail(
+                paymentConfirmation.customerEmail(),
+                customerName,
+                paymentConfirmation.amount(),
+                paymentConfirmation.orderReference()
+
+        );
 
     }
 
     @KafkaListener(topics = "order-topic")
-    public void consumeConfirmationNotification(OrderConfirmation orderConfirmation){
+    public void consumeConfirmationNotification(OrderConfirmation orderConfirmation) throws MessagingException {
         log.info(String.format("Consuming the message from payment-topic Topic:: %s", orderConfirmation));
         repository.save(
                 Notification.builder()
@@ -54,6 +64,15 @@ public class NotificationConsumer {
         );
 
         // to do send email
+        var customerName = orderConfirmation.customer().firstname() + " " + orderConfirmation.customer().lastname();
+        emailService.sendOrderConfirmationEmail(
+                orderConfirmation.customer().email(),
+                customerName,
+                orderConfirmation.totalAmount(),
+                orderConfirmation.orderReference(),
+                orderConfirmation.products()
+
+        );
 
     }
 
