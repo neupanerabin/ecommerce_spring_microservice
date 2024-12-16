e87c937af16ec10a02364383795e16d34a2e0a95
neupanerabin7@gmail.com <neupanerabin7@gmail.com>
2024-12-16 23:22:11 +0545

    add amount

--- services/payment/src/main/java/org/rabin/ecommerce/payment/PaymentService.java
+++ services/payment/src/main/java/org/rabin/ecommerce/payment/PaymentService.java
@@ -18,10 +18,12 @@ public class PaymentService {
     private final NotificationProducer notificationProducer;
 
     public Integer createPayment(PaymentRequest request) {
-        var payment = repository.save(mapper.toPayment(request));
-        notificationProducer.sendNotification(
+        var payment = this.repository.save(this.mapper.toPayment(request));
+
+        this.notificationProducer.sendNotification(
                 new PaymentNotificationRequest(
-                        request.orderRefrence(),
+                        request.orderReference(),
+                        request.amount(),
                         request.paymentMethod(),
                         request.customer().firstname(),
                         request.customer().lastname(),
@@ -29,7 +31,6 @@ public class PaymentService {
 
                 )
         );
-
         return payment.getId();
     }
 }
