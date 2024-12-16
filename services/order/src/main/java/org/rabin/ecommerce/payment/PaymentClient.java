d829c7c8037111f140f280d771682d6789432a88
neupanerabin7@gmail.com <neupanerabin7@gmail.com>
2024-12-16 22:22:34 +0545

    change name

--- services/order/src/main/java/org/rabin/ecommerce/payment/PaymentClient.java
+++ services/order/src/main/java/org/rabin/ecommerce/payment/PaymentClient.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(
-        name = "product--service",
+        name = "product-service",
         url = "${application.config.payment-url}"
 )
 public interface PaymentClient {
