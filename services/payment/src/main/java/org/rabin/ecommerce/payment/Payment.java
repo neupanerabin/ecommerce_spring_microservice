040441b3cbc83092acae447e138de09d9c39575a
neupanerabin7@gmail.com <neupanerabin7@gmail.com>
2024-12-17 23:05:46 +0545

    add Entity annotation

--- services/payment/src/main/java/org/rabin/ecommerce/payment/Payment.java
+++ services/payment/src/main/java/org/rabin/ecommerce/payment/Payment.java
@@ -5,37 +5,54 @@ package org.rabin.ecommerce.payment;
  * @author : rabin
  */
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-
-import static jakarta.persistence.EnumType.STRING;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
 @Setter
 @Getter
+@Builder
+@Entity
 @EntityListeners(AuditingEntityListener.class)
 @Table(name = "payment")
-public class Payment {
+public class Payment{
 
+    @Getter
     @Id
     @GeneratedValue
     private Integer id;
+
     private BigDecimal amount;
-    @Enumerated(STRING)
+
+    @Enumerated(EnumType.STRING)
     private PaymentMethod paymentMethod;
+
     private Integer orderId;
     @CreatedDate
     @Column(updatable = false, nullable = false)
     private LocalDateTime createdDate;
+
     @LastModifiedDate
     @Column(insertable = false)
     private LocalDateTime lastModifiedDate;
+
+
 }
